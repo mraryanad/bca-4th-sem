9443e7ab07c0f2778bbcc8e945833ac77376f522
AryanAD <aryanhohai@gmail.com>
2024-08-07 13:21:57 +0545

    initial commit

---
+++ scripting/index.js
@@ -0,0 +1,22 @@
+// function add() {
+//   let a = 1;
+//   let b = 2;
+//   let result = a + b;
+//   return result;
+// }
+
+// function sub(a, b) {
+//   return (result = a - b);
+// }
+
+// function greet() {
+//   return console.log("Hello BCA");
+// }
+
+// console.log(add());
+// console.log(sub(5, 1));
+// greet();
+
+const handleClickExternal = () => {
+  alert("External JS");
+};
