8cedfae8809795ce3c178af68a7a7caab5b63a2c
AryanAD <aryanhohai@gmail.com>
2024-08-26 14:54:32 +0545

    push

---
+++ scripting/day8/index.js
@@ -0,0 +1,25 @@
+numbers = [1, 2, 3, 4, 5];
+
+// numbers.map((num) => {
+//     console.log(typeof num);
+// });
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+a = 10;
+while (a > 0) {
+  a--;
+  console.log(a);
+}
+
+do {
+  a--;
+  console.log(a);
+} while (a > 0);
+
+console.log("\n\n\nTable of 2:");
+
+for (i = 1; i < 11; i++) {
+  console.log(2 + " * " + i + ": " + 2 * i);
+}
---
+++ scripting/day9/index.js
@@ -0,0 +1,4 @@
+let myArray = ["Hello", "World", "This", "Is", "My", "Array"];
+myArray.map((item) => {
+  console.log(item);
+});
